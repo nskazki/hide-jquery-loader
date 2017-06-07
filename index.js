73cb52549577c38979c7eb6e2016ff0ecd7fb88c
nskazki <nskazki@gmail.com>
2017-06-07 14:18:48 +0300

    simple hack to avoid source map breaking

--- index.js
+++ index.js
@@ -2,26 +2,28 @@
 
 let _uniqueId = 0
 const uniqueId = () => ++_uniqueId
+const deline = str => str.replace(/[\r\n]/g, ' ').replace(/\s{2,}/, ' ')
 
 module.exports = function hideJqueryLoader(source) {
   this.cacheable()
   const $Orig = `__$__orig__${uniqueId()}`
   const jQueryOrig = `__jQuery__orig__${uniqueId()}`
 
-  return `
-/* HIDE JQUERY LOADER -- https://github.com/nskazki/hide-jquery-loader */
+  const header = deline(`
+    /* HIDE JQUERY LOADER HEADER -- https://github.com/nskazki/hide-jquery-loader */
+    var _window;
+    try { _window = Function('return this')() || (42, eval)('this'); }
+    catch (_err) { _window = window || global || GLOBAL || {}; }
 
-var _window
-try { _window = Function('return this')() || (42, eval)('this'); }
-catch (_err) { _window = window || global || GLOBAL || {}; }
+    var ${$Orig} = _window.$;
+    var ${jQueryOrig} = _window.jQuery;
+    delete _window.$;
+    delete _window.jQuery;`)
 
-var ${$Orig} = _window.$;
-var ${jQueryOrig} = _window.jQuery;
-delete _window.$;
-delete _window.jQuery;
+  const footer = deline(`
+    /* HIDE JQUERY LOADER FOOTER -- https://github.com/nskazki/hide-jquery-loader */
+    _window.$ = ${$Orig};
+    _window.jQuery = ${jQueryOrig};`)
 
-${source};
-
-_window.$ = ${$Orig};
-_window.jQuery = ${jQueryOrig};`
+  return `${header} ${source}; ${footer}`
 }
